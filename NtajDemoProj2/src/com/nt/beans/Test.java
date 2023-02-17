4995e1cbf58322d5728fa9592361ab2501370c4c
TL <TL@gmail.com>
2023-02-17 15:29:52 +0530

    dev1 add m2() method

--- NtajDemoProj2/src/com/nt/beans/Test.java
+++ NtajDemoProj2/src/com/nt/beans/Test.java
@@ -1,8 +1,14 @@
 package com.nt.beans;
 
+import java.util.Date;
+
 public class Test {
    public void m1() {
 	   System.out.println("Chiranjibi");
 	   System.out.println("dash");
+   }
+   public void m2() {
+	   Date date = new Date();
+	   System.out.println(date);
    }
 }
